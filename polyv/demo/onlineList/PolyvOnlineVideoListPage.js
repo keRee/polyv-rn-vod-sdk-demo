3b9529a0ae26b99b58fdcc7de7c164faf55ea3a7
daifan <daifan@corp.polyv.net>
2019-03-26 18:10:11 +0800

    用户信息集中配置在userconfig 里

--- polyv/demo/onlineList/PolyvOnlineVideoListPage.js
+++ polyv/demo/onlineList/PolyvOnlineVideoListPage.js
@@ -10,6 +10,7 @@ import { setAxios } from "../common/PolyvNet";
 
 import PolyvVodConfigRnModule from "../../sdk/PolyvVodConfigRnModule";
 import PolyvVideoListView from "./view/PolyvVideoOnlineList";
+import PolyvUserConfig from '../PolyvUserConfig'
 
 
 const { width, height } = Dimensions.get("window");
@@ -37,15 +38,14 @@ export default class PolyvOnlineVideoListPage extends Component {
     super(props);
     this.state = {
       // 初始化所需的数据
-      vodKey:
-        "yQRmgnzPyCUYDx6weXRATIN8gkp7BYGAl3ATjE/jHZunrULx8CoKa1WGMjfHftVChhIQlCA9bFeDDX+ThiuBHLjsNRjotqxhiz97ZjYaCQH/MhUrbEURv58317PwPuGEf3rbLVPOa4c9jliBcO+22A==",
-      decodeKey: "VXtlHmwfS2oYm0CZ",
-      decodeIv: "2u9gDPKdX6GyQJKU",
-      viewerId: "rn_viewerId",
-      nickName: "rn_nickName",
+      vodKey:PolyvUserConfig.User.vodKey,
+      decodeKey: PolyvUserConfig.User.decodeKey,
+      decodeIv: PolyvUserConfig.User.decodeIv,
+      viewerId: PolyvUserConfig.User.viewerId,
+      nickName: PolyvUserConfig.User.nickName,
 
       // 输入框默认vid
-      inputVid: "e97dbe3e649c56a1e58535bd8c5d3924_e",
+      inputVid: PolyvUserConfig.User.inputVid,
       canDownload: false
     };
   }
